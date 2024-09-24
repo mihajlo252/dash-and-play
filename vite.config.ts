1d067c81557a5e92fbcb38c255a375e77881cf8d
Mihajlo Kostic <mihajlo.kostic25200@gmail.com>
2024-09-24 21:57:51 +0200

    Add images and config for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
-  base: '/dash-and-play/',
+  base: '/dash-and-play',
 })
